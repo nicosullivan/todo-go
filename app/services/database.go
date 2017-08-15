19ba722a38b7d90b58b53154cc4f7be31c6031ac
Nic O'Sullivan <nic@osullivan.tech>
2017-08-14 20:52:06 -0500

    moved database connection config to ENV

--- app/services/database.go
+++ app/services/database.go
@@ -1,33 +1,47 @@
 package services
 
 import (
+	"fmt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/nicosullivan/todo-go/app/models"
 	"github.com/revel/revel"
+	"os"
 )
 
 type Database struct {
 	Orm *gorm.DB
 }
 
-func InitDB() {
-	db, err := gorm.Open("postgres", "host=localhost user=postgres dbname=gorm sslmode=disable password=postgres")
+func ConnectToDatabase() *gorm.DB {
+	connectionString := fmt.Sprintf(
+		"host=%s user=%s dbname=%s sslmode=%s password=%s",
+		os.Getenv("TODOHOST"),
+		os.Getenv("TODOUSER"),
+		os.Getenv("TODONAME"),
+		os.Getenv("TODOSSL"),
+		os.Getenv("TODOPASSWORD"))
+
+	db, err := gorm.Open("postgres", connectionString)
 	if err != nil {
 		panic(err)
 	}
+
 	db.SingularTable(true)
 
+	return db
+}
+
+func InitDB() {
+	db := ConnectToDatabase()
+
 	db.AutoMigrate(&models.Todo{})
+
+	db.Close()
 }
 
 func (c *Database) Open() revel.Result {
-	var err error
-	c.Orm, err = gorm.Open("postgres", "host=localhost user=postgres dbname=gorm sslmode=disable password=postgres")
-	if err != nil {
-		panic(err)
-	}
-	c.Orm.SingularTable(true)
+	c.Orm = ConnectToDatabase()
 	return nil
 }
 
